3ee7dd883f44b36746872813e80c26448843acdc
Herb Torres <herb.torres@byhook.com>
2016-12-16 10:34:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 25K Challenge - Top Down Christmas Theme Canvas Game
+
+25k Challenge - Do something cool, make it under 25K file weight.
---
+++ app/index.js
@@ -0,0 +1,204 @@
+'use strict';
+
+// Handle keyboard controls
+var keysDown = {};
+
+var Game = function() {
+
+	this.canvas = document.querySelector('#game');
+	this.ctx = this.canvas.getContext('2d');
+	this.canvas.width = 480;
+	this.canvas.height = 800;
+	// this.canvas.width = window.innerWidth;
+	// this.canvas.height = window.innerHeight;
+	
+	document.body.appendChild(this.canvas);
+
+	addEventListener('keydown', function (e) {
+		keysDown[e.keyCode] = true;
+	}, false);
+
+	addEventListener('keyup', function (e) {
+		delete keysDown[e.keyCode];
+	}, false);
+
+	// initialize
+	this.init();
+
+	window.addEventListener('resize', this._onResize.bind(this));
+}
+
+Game.prototype.init = function() {
+
+	this.drawBackground();
+	this.drawSanta();
+	this.drawGift();
+	this.dPad();
+
+	this.then = Date.now();
+
+	// Game objects
+	this.santaCharacter = {
+		speed: 256 // movement in pixels per second
+	};
+	this.giftToGet = {};
+	this.giftsCollected = 0;
+
+	this.santaCharacter.x = this.canvas.width / 2;
+	this.santaCharacter.y = this.canvas.height / 2;
+
+
+	this.setRandomGiftLocation();
+	this.animate();
+};
+
+
+Game.prototype.drawBackground = function() {
+
+	this.bgImage = new Image();
+	this.bgImage.src = 'images/ice-x.jpg';
+
+};
+
+Game.prototype.drawSanta = function() {
+
+	this.santa = new Image();
+	this.santa.src = 'images/santa.png';
+};
+
+Game.prototype.drawGift = function() {
+
+	this.gift = new Image();
+	this.gift.src = 'images/gift.png';
+};
+
+
+// Throw the new gift somewhere on the screen randomly
+Game.prototype.setRandomGiftLocation = function () {
+
+	// this.santaCharacter.x = this.canvas.width / 2;
+	// this.santaCharacter.y = this.canvas.height / 2;
+	this.giftToGet.x = 32 + (Math.random() * (this.canvas.width - 64));
+	this.giftToGet.y = 32 + (Math.random() * (this.canvas.height - 64));
+};
+
+// Update game objects
+Game.prototype.update = function (modifier) {
+
+	if (38 in keysDown) { // Player holding up
+		if (this.santaCharacter.y >= 0) {
+			this.santaCharacter.y -= this.santaCharacter.speed * modifier;
+		};
+	}
+	if (40 in keysDown) { // Player holding down
+		if (this.santaCharacter.y <= this.canvas.height - 64) {
+			this.santaCharacter.y += this.santaCharacter.speed * modifier;
+		};
+	}
+	if (37 in keysDown) { // Player holding left
+		if (this.santaCharacter.x >= 0) {
+			this.santaCharacter.x -= this.santaCharacter.speed * modifier;
+			// TweenLite.to(this.santaCharacter, 0.3, {scale: 180});
+			this.santa.src = 'images/santaL.png';
+		};
+	}
+	if (39 in keysDown) { // Player holding right
+		if (this.santaCharacter.x <= (this.canvas.width - 32)) {
+			this.santaCharacter.x += this.santaCharacter.speed * modifier;
+			// TweenLite.to(this.santaCharacter, 0.3, {scale: 0});
+			this.santa.src = 'images/santa.png';
+		};
+	}
+
+	// collision detection
+	if (this.santaCharacter.x <= (this.giftToGet.x + 32) && this.giftToGet.x <= (this.santaCharacter.x + 32) && this.santaCharacter.y <= (this.giftToGet.y + 32)
+ && this.giftToGet.y <= (this.santaCharacter.y + 32)) {
+
+		if (this.giftsCollected < 9) {
+			this.giftsCollected++;
+			this.setRandomGiftLocation();
+		} else {
+			// document.querySelector('.end-game').classList.add('reveal-end-game');
+			var overlay = document.querySelector('.end-game');
+			TweenLite.to(overlay, 0.6, {
+				opacity: 1,
+				zIndex: 5,
+				ease: Power4.easeOut
+			});
+		};
+	}
+};
+
+// Draw everything
+Game.prototype.render = function () {
+
+	this.ctx.drawImage(this.bgImage, 0, 0);
+	// var ptrn = this.ctx.createPattern(this.bgImage, 'repeat'); // Create a pattern with this image, and set it to 'repeat'.
+ 	// this.ctx.fillStyle = ptrn;
+    // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	this.ctx.drawImage(this.santa, this.santaCharacter.x, this.santaCharacter.y);
+	this.ctx.drawImage(this.gift, this.giftToGet.x, this.giftToGet.y);
+
+	// Score
+	this.ctx.fillStyle = 'rgb(152, 0, 0)';
+	this.ctx.font = '24px Roboto';
+	this.ctx.textAlign = 'left';
+	this.ctx.textBaseline = 'top';
+	this.ctx.fillText('Presents Collected: ' + this.giftsCollected, 32, 32);
+};
+
+// The main game loop
+Game.prototype.animate = function () {
+	var now = Date.now();
+	var delta = now - this.then;
+
+	this.update(delta / 1000);
+	this.render();
+
+	this.then = now;
+
+	requestAnimationFrame(this.animate.bind(this));
+};
+
+// d pad movement
+Game.prototype.dPad = function() {
+	// mobile d pad movement
+	var top = document.querySelector('#top');
+	var left = document.querySelector('#left');
+	var right = document.querySelector('#right');
+	var bottom = document.querySelector('#down');
+
+	top.addEventListener('mousedown', function() {
+		if (this.santaCharacter.y >= 0) {
+			this.santaCharacter.y -= this.santaCharacter.speed * 0.1;
+		};
+	}.bind(this));
+
+	down.addEventListener('mousedown', function() {
+		if (this.santaCharacter.y <= this.canvas.height - 64) {
+			this.santaCharacter.y += this.santaCharacter.speed * 0.1;
+		};
+	}.bind(this));
+
+	left.addEventListener('mousedown', function() {
+		if (this.santaCharacter.x >= 0) {
+			this.santaCharacter.x -= this.santaCharacter.speed * 0.1;
+			// TweenLite.to(this.santaCharacter, 0.3, {scale: 180});
+			this.santa.src = 'images/santaL.png';
+		};
+	}.bind(this));
+
+	right.addEventListener('mousedown', function() {
+		if (this.santaCharacter.x <= (this.canvas.width - 32)) {
+			this.santaCharacter.x += this.santaCharacter.speed * 0.1;
+			// TweenLite.to(this.santaCharacter, 0.3, {scale: 0});
+			this.santa.src = 'images/santa.png';
+		};
+	}.bind(this));
+}
+
+Game.prototype._onResize = function() {
+	this.render();
+};
+
+var game = new Game();
